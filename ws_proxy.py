11a48f466916c7545ed83bbd2e1555d6c66fe351
nowzycc <2366834066@qq.com>
2024-01-03 15:37:30 +0800

    将代理模式修改为异步IO

--- ws_proxy.py
+++ ws_proxy.py
@@ -11,25 +11,39 @@ ws_server_entry_point = config['ws_server_entry_point']
 game_server_port = config['game_server_port']
 game_server_protrocl = config['game_server_protrocl']
 
-async def main():
+socket_handle = None
+websocket_handle = None
+
+async def socket2ws_handle(loop:asyncio.AbstractEventLoop,wait_ws:asyncio.Semaphore,wait_udp:asyncio.Semaphore):
+    await wait_ws.acquire()
+    global socket_handle
+    if game_server_protrocl == 'udp':
+        socket_handle = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        local_addr = ("", game_server_port)
+        socket_handle.bind(local_addr)
+    wait_udp.release()
+    while True:
+        data = await loop.sock_recv(socket_handle,1024)
+        await websocket_handle.send(data)
+
+async def ws2socket_handle(loop:asyncio.AbstractEventLoop,wait_ws:asyncio.Semaphore,wait_udp:asyncio.Semaphore):
     async with websockets.connect(ws_server_entry_point) as websocket:
-        s = socket.socket()         # 创建 socket 对象
-        print(s.getblocking())
-        host = socket.gethostname() # 获取本地主机名
-        s.setblocking(True)
-        port = game_server_port                # 设置端口
-        s.bind((host, port))        # 绑定端口
-        s.listen(5) 
-        c,addr = s.accept()
+        global websocket_handle
+        websocket_handle = websocket
+        wait_ws.release()
+        await wait_udp.acquire() #等待udp句柄创建
         while True:
-            data = c.recv(1500)
-            message = data
-            print(message)
-            await websocket.send(message)
-            print(f"Sent: {message}")
-            response = await websocket.recv()
-            c.send(response)
-            print(f"Received: {response}")
+            data = await websocket.recv()
+            await loop.sock_sendall(socket_handle,data)
 
 
-asyncio.run(main())
+def main():
+    semp_wait_ws = asyncio.Semaphore(0)
+    semp_wait_udp = asyncio.Semaphore(0)
+    loop = asyncio.get_event_loop()
+    loop.create_task(socket2ws_handle(loop=loop,wait_ws=semp_wait_ws,wait_udp=semp_wait_udp))
+    loop.create_task(ws2socket_handle(loop=loop,wait_ws=semp_wait_ws,wait_udp=semp_wait_udp))
+    loop.run_forever()
+
+
+main()
